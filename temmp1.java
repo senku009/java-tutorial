c3c15e460abf6b7a5b802b32069c2e061a27cdec
senku009 <vegetaisnotover9000@gmail.com>
2023-07-04 13:31:58 +0530

    2 commit

--- temmp1.java
+++ temmp1.java
@@ -5,6 +5,9 @@ public class temmp1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("heelo this is temmp1");
+		System.out.print("this is a 2 commit");
+		System.out.print("2 commit as well");
+		
 	}
 
 }
